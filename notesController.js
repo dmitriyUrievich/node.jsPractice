da8211dbcfaf069416a45e7780bd91d564dd86ce
dmitriyUrievich <dmitriy.bubenin@gmail.com>
2022-11-25 00:04:47 +0300

    less1.task

--- notesController.js
+++ notesController.js
@@ -6,14 +6,13 @@ const notesPath = path.join(__dirname,'db.json')
 async function addNote (title) {
   // const notes = await fs.readFile(notesPath,encoding='utf-8')
   // const buffer = Buffer.from(notes).toString('utf-8')
-
   const notes = await getNote()
   const note = {
     title,
     id: Date.now().toString()
   }
 notes.push(note)
-await fs.writeFile(notesPath,JSON.stringify(notes))
+  await saveNotes(notes);
   console.log(chalk.green('Note was added'))
 }
 
@@ -25,11 +24,23 @@ async function getNote () {
 async function printNotes(){
 const notes = await getNote()
   console.log(chalk.bgBlue('List of notes'))
-    notes.forEach(note=>console.log(chalk.blue(note.title)))
+    notes.forEach(note=>console.log(chalk.blue(note.id,note.title)))
+}
+
+async function saveNotes(notes) {
+  await fs.writeFile(notesPath, JSON.stringify(notes));
+}
+async function removeNotes(id){
+  const notes = await getNote()
+  const newNotes = notes.filter(x =>x.id !== id)
+  console.log(chalk.red('Remove noteID', id))
+  await saveNotes(newNotes)
+  return newNotes
 }
 
 module.exports ={
   addNote,
-  printNotes
+  printNotes,
+  removeNotes
 }
 
